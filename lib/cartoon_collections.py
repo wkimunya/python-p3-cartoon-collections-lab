095566a84b740032ccda92dd9f2bc6fa8c172a13
wkimunya <136619749+wkimunya@users.noreply.github.com>
2023-10-31 09:37:35 +0300

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,17 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, 1):
+        print(f"{i}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    captain_planet_calls = [call.capitalize() + '!' for call in planeteer_calls]
+    return captain_planet_calls
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
 
-def find_the_cheese():
-    pass
+def find_the_cheese(items):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    for item in items:
+        if item in cheese_types:
+            return item
+    return None
